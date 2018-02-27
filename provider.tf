8b3620a182b6a45ef63ce4556c87bc37b51ac455
nado <nfaqou@mediametrie.fr>
2018-02-27 17:16:27 +0100

    update provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "${var.region}"
+  access_key = "AKIAJKZHTIKQSOMOYWWQ"
+  secret_key = "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"
   assume_role {
     role_arn = "${var.role_arn}"
   }
